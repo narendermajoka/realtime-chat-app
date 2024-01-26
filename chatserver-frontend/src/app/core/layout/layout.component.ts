d64587ff23780917ac01b977e5b4d86760c296ad
Narender Majoka <narendermjk.33@gmail.com>
2024-01-26 21:17:23 +0530

    working real time chat app frontend and backend both

--- chatserver-frontend/src/app/core/layout/layout.component.ts
+++ chatserver-frontend/src/app/core/layout/layout.component.ts
@@ -8,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class LayoutComponent implements OnInit {
 
   constructor() { }
-
+  userFullName;
   ngOnInit() {
+    this.userFullName = localStorage.getItem('userFullName');
   }
 
 }
